3b6c4fb2871543a64c8210adb686debbc0915b8c
Shomen Banerjee <shomenbanerjee@hotmail.com>
2014-10-26 16:11:22 +0000

    Update cachematrix.R
    added comments to the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this code is to cache an inverse of matrix
+## 'makeCacheMatrix' offers methods to set and get matrix inverse. While
+## cacleSolve computes the matrix inverse in case it is not already availble
+## in the cache
 
-## Write a short comment describing this function
+## makeCacheMatrix allows set & get of the matrix, while inc_Matrix has the
+## inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
        
        inv_Matrix <- matrix()
        
+       #set and get matrix
        set <- function(y = matrix()) {
               x <<- y              
               inv_Matrix <<- NULL              
        }
 
+       
        get <- function() x
        
        
@@ -25,17 +30,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve retrieves inverse matrix from the cache, and in case not
+## found, computes and stores it for future use
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+       ## Return a matrix that is the inverse of 'x'
        inv_Matrix <- x$getInvMatrix()
        
+       #check if inverse matrix is available from cache
        if(!is.null(inv_Matrix)){
               print("This is cached data")
               return(inv_Matrix)
        }
        
+       # Inverse matrix is not available, hence compute and cache it
        matrix <- x$get()
        inv_Matrix <- solve(matrix)
        x$setInvMatrix(inv_Matrix)
